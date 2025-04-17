b34d04a7eff40bcddebff2295e2b19b439bc730b
Meme Houeibib <38425746+memehoueibib@users.noreply.github.com>
2025-04-17 13:13:34 +0200

    init all

---
+++ ModernSoftwareApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿using Xunit;
+using ModernSoftwareApp;
+
+namespace ModernSoftwareApp.Tests
+{
+    public class CalculatorTests
+    {
+        [Fact]
+        public void Add_ShouldReturnCorrectSum()
+        {
+            var calc = new Calculator();
+            Assert.Equal(5, calc.Add(2, 3));
+        }
+    }
+}
